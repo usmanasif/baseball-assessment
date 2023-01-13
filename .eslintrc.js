0ca1646d4dc50eba425fedecf722bdb54aa78c09
Usman Asif <pyroxicated@gmail.com>
2023-01-12 16:19:02 -0800

    Install Packages, Configure Eslint & husky

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'dot-notation': 0,
+    'quote-props': 1,
+    'curly': 0
+  }
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+// Third Party Packages Imports
+const express = require('express')
+
+// Custom Imports
+const routes = require('./src/routes')
+
+const app = express()
+const port = 3000
+
+// Middleware
+app.use(express.json())
+
+// Setting Headers
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'api-token, Origin, Content-Type, Accept'
+  )
+  next()
+})
+
+// Routes
+routes(app)
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
